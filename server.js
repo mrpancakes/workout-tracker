3f208646b5df53ca3c76c4501e85466d89e289a3
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-06-04 14:47:38 -0500

    Updated server file

--- server.js
+++ server.js
@@ -4,4 +4,26 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = 
+const db = require('./models');
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { 
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+});
+
+app.use(require('./routes/api'));
+app.use(require('./routes/view'));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
